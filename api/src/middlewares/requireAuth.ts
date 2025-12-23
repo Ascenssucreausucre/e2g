64f0916ef8c6668491f48b8f24fe6e1f46f4a997
unknown <amiotflorian33@gmail.com>
2025-12-23 19:53:02 +0100

    debug

--- api/src/middlewares/requireAuth.ts
+++ api/src/middlewares/requireAuth.ts
@@ -2,12 +2,16 @@ import { verifyJwt } from "../utils/jwt";
 import { Request, Response, NextFunction } from "express";
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  console.log("Authenticating request...");
   const token = req.cookies.auth;
+  console.log("Token found:", !!token);
   if (!token) return res.sendStatus(401);
 
   const user = verifyJwt<{ userId: string }>(token);
+  console.log("Token valid:", !!user);
   if (!user) return res.sendStatus(401);
 
   req.user = user;
+  console.log("Authentication successful for user:", user.userId);
   next();
 }
